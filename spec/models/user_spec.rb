d61e501b650aa3e98ae8fb5656567001de8de4f1
JL <lab.galom@outlook.com>
2018-10-01 01:00:47 +0300

    korjauksia

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,13 +27,9 @@ RSpec.describe User, type: :model do
     expect(User.count).to eq(0)
   end
   it "is the one with highest rating if several rated" do
-    beer1 = FactoryBot.create(:beer)
-    beer2 = FactoryBot.create(:beer)
-    beer3 = FactoryBot.create(:beer)
-    rating1 = FactoryBot.create(:rating, score: 20, beer: beer1, user: user)
-    rating2 = FactoryBot.create(:rating, score: 25, beer: beer2, user: user)
-    rating3 = FactoryBot.create(:rating, score: 9, beer: beer3, user: user)
-
+    create_beer_with_rating({ user: user }, 10 )
+    create_beer_with_rating({ user: user}, 7 )
+    best = create_beer_with_rating({ user: user }, 25 )
     expect(user.favorite_beer).to eq(beer2)
   end
 
@@ -46,36 +42,34 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
       expect(User.count).to eq(1)
     end
-
+  end
     it "has method for determining the favorite_beer" do
       user = FactoryBot.create(:user)
       expect(user).to respond_to(:favorite_beer)
     end
     describe "favorite beer" do
-      let(:user){ FactoryBot.create(:user) }
+    let(:user){ FactoryBot.create(:user) }
 
-      it "has method for determining one" do
-        expect(user).to respond_to(:favorite_beer)
+    it "has method for determining one" do
+      expect(user).to respond_to(:favorite_beer)
     end
 
     it "without ratings does not have one" do
-        expect(user.favorite_beer).to eq(nil)
+      expect(user.favorite_beer).to eq(nil)
     end
+
     it "is the only rated if only one rating" do
       beer = FactoryBot.create(:beer)
       rating = FactoryBot.create(:rating, score: 20, beer: beer, user: user)
 
       expect(user.favorite_beer).to eq(beer)
     end
-    it "and with two ratings, has the correct average rating" do
-      rating = Rating.new score: 10, beer: test_beer
-      rating2 = Rating.new score: 20, beer: test_beer
 
-      user.ratings << rating
-      user.ratings << rating2
+    it "is the one with highest rating if several rated" do
+      create_beers_with_many_ratings({user: user}, 10, 20, 15, 7, 9)
+      best = create_beer_with_rating({ user: user }, 25 )
 
-      expect(user.ratings.count).to eq(2)
-      expect(user.average_rating).to eq(15.0)
+      expect(user.favorite_beer).to eq(best)
     end
   end
 end
