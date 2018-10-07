f3710c4e6a769e232478adff76d9e9e8a5daa5a6
JL <lab.galom@outlook.com>
2018-10-07 20:34:29 +0300

    teht2viikko5

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :breweries
   resources :ratings, only: [:index, :new, :create, :destroy]
   resource :session, only: [:new, :create, :destroy]
+  resources :places, only: [:index, :show]
 
   get 'signup', to: 'users#new'
   get 'signin', to: 'sessions#new'
